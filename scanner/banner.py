61befb499ca66eb106ab37178ab889cf9ed4579c
Rushikesh_Bhujbal <maharajrushi700@gmail.com>
2026-04-05 17:42:20 +0530

    Add banner grabbing functionality

---
+++ scanner/banner.py
@@ -0,0 +1,25 @@
+import socket
+
+def grab_banner(target_ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+
+        sock.connect((target_ip, port))
+
+        banner = sock.recv(1024).decode(errors="ignore").strip()
+
+        sock.close()
+
+        return banner
+
+    except socket.error:
+        return ""
+
+
+
+if __name__ == "__main__":
+    ip = socket.gethostbyname("scanme.nmap.org")
+
+    print("80:", grab_banner(ip, 80))
+    print("22:", grab_banner(ip, 22))
